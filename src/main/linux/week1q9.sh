dfd6bac2b9f2067a1f026bec68cc1cdc1e9cd046
Priyanka_Hatgine <patil.priyanka885@gmail.com>
2021-12-01 16:25:46 +0530

    Initial commit

---
+++ src/main/linux/week1q9.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+#function sleeper
+
+function sleeper()
+{                            # define a function that...
+    sleep $1                 # sleeps for duration of its argument in seconds
+    echo $1                  # prints its argument to the console
+}
+while [ -n "$1" ]
+do
+    sleeper $1 &
+    shift
+done
+wait
---
+++ src/main/linux/week2q3.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+if java -version;
+then
+      echo "Java is installed"
+      if  [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin" ]];
+      then
+             echo "found java executable in JAVA_HOME"
+             echo "JAVA_HOME : " $JAVA_HOME
+             echo "Path : " $PATH
+      else
+            echo "JAVA_HOME  env variable is not set"
+            java_path= which java
+            echo "$java_path"
+            export JAVA_HOME=$java_path
+            export PATH=$JAVA_HOME/bin
+            echo "$JAVA_HOME"
+            echo "$PATH"
+      fi
+else
+      echo "java is not installed"
+      sudo apt update
+      sudo apt install oracle-java11-installer
+      java -version
+      echo "java is installed on your system"
+fi
